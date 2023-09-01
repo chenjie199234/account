cc4ac4e4f3737a864b6c9451cb8fb7486729e97d
chenjie199234 <chenjie199234@gmail.com>
2023-09-01 20:00:39 +0800

    update

--- dao/user/redis.go
+++ dao/user/redis.go
@@ -2,134 +2,206 @@ package user
 
 import (
 	"context"
-	"crypto/sha1"
-	"encoding/hex"
-	"strings"
+	"encoding/json"
+
+	"github.com/chenjie199234/account/ecode"
+	"github.com/chenjie199234/account/model"
 
 	"github.com/chenjie199234/Corelib/redis"
-	"github.com/chenjie199234/account/ecode"
 )
 
-const DefaultExpireSeconds = 300
-const DefaultCheckTimes = 5
-
-func init() {
-	h := sha1.Sum([]byte(setcode))
-	hsetcode = hex.EncodeToString(h[:])
-	h = sha1.Sum([]byte(checkcode))
-	hcheckcode = hex.EncodeToString(h[:])
-}
-
-func rediskey(target, action string) string {
-	return target + "_" + action + "_code"
-}
-
-// argv 1 = code
-// argc 2 = max check times
-// argc 3 = expire time(unit second)
-// return -1 = set success
-// return >=0 = already setted before,data is the rest check times
-const setcode = `local used=redis.call("HGET",KEYS[1],"check")
-if(used~=nil)
-then
-	return ARGV[2]-used
-end
-redis.call("HMSET",KEYS[1],"code",ARGV[1],"check",0)
-redis.call("EXPIRE",KEYS[1],ARGV[2])
-return -1`
-
-var hsetcode string
-
-func (d *Dao) RedisSetCode(ctx context.Context, target, action string, code string) (int, error) {
-	c, e := d.redis.GetContext(ctx)
-	if e != nil {
-		return 0, e
-	}
-	defer c.Close()
-	var rest int
-	rest, e = redis.Int(c.DoContext(ctx, "EVALSHA", hsetcode, 1, rediskey(target, action), code, DefaultCheckTimes, DefaultExpireSeconds))
-	if e != nil && strings.Contains(e.Error(), "NOSCRIPT") {
-		rest, e = redis.Int(c.DoContext(ctx, "EVAL", setcode, 1, rediskey(target, action), code, DefaultCheckTimes, DefaultExpireSeconds))
-	}
-	if e != nil {
-		return 0, e
-	}
-	if rest != -1 {
-		e = ecode.ErrCodeAlreadySend
-	} else {
-		rest = DefaultCheckTimes
-	}
-	return rest, e
-}
-
-// argv 1 = code
-// argv 2 = max check times
-// argv 3 = expire time(unit second)
-// return nil = key already expired
-// return number = rest check times
-// -1 means code same,check success
-// 0 means all check times used,this key will be expired after expire time
-const checkcode = `local data=redis.call("HMGET",KEYS[1],"code","check")
-if(data[1]==nil or data[2]==nil)
-then
-	return nil
-end
-if(data[1]==ARGV[1] and data[2]<ARGV[2])
-then
-	redis.call("DEL",KEYS[1])
-	return -1
-end
-if(data[2]>=ARGV[2])
-then
-	return 0
-end
-if(redis.call("EXPIRE",KEYS[1],ARGV[2])==0)
-then
-	return nil
-end
-data[2]=redis.call("HINCRBY",KEYS[1],"check",1)
-return ARGV[2]-data[2]`
-
-var hcheckcode string
-
-func (d *Dao) RedisCheckCode(ctx context.Context, target, action string, code string) (int, error) {
-	c, e := d.redis.GetContext(ctx)
-	if e != nil {
-		return 0, e
-	}
-	defer c.Close()
-	var rest int
-	rest, e = redis.Int(c.DoContext(ctx, "EVALSHA", hcheckcode, 1, rediskey(target, action), code, DefaultCheckTimes, DefaultExpireSeconds))
-	if e != nil && strings.Contains(e.Error(), "NOSCRIPT") {
-		rest, e = redis.Int(c.DoContext(ctx, "EVAL", checkcode, 1, rediskey(target, action), code, DefaultCheckTimes, DefaultExpireSeconds))
-	}
-	if e == redis.ErrNil {
-		e = ecode.ErrCodeAlreadyExpire
-	}
-	return rest, e
-}
-func (d *Dao) RedisGetCode(ctx context.Context, target, action string) (string, int, error) {
-	c, e := d.redis.GetContext(ctx)
-	if e != nil {
-		return "", 0, e
-	}
-	defer c.Close()
-	values, e := redis.Values(c.DoContext(ctx, "HMGET", rediskey(target, action), "code", "check"))
-	if e != nil {
-		if e == redis.ErrNil {
-			e = ecode.ErrCodeAlreadyExpire
-		}
-		return "", 0, e
-	}
-	return values[0].(string), values[1].(int), nil
-}
-
-func (d *Dao) RedisDelCode(ctx context.Context, target, action string) error {
+func (d *Dao) RedisSetUser(ctx context.Context, userid string, user *model.User) error {
 	c, e := d.redis.GetContext(ctx)
 	if e != nil {
 		return e
 	}
 	defer c.Close()
-	_, e = c.DoContext(ctx, "DEL", rediskey(target, action))
+	if user == nil || user.UserID.IsZero() {
+		//set empty key
+		_, e = redis.String(c.DoContext(ctx, "SET", userid, "", "EX", 604800))
+	} else {
+		data, _ := json.Marshal(user)
+		_, e = redis.String(c.DoContext(ctx, "SET", userid, data, "EX", 604800))
+	}
+	return e
+}
+func (d *Dao) RedisGetUser(ctx context.Context, userid string) (*model.User, error) {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return nil, e
+	}
+	defer c.Close()
+	data, e := redis.Bytes(c.DoContext(ctx, "GET", userid))
+	if e != nil {
+		if e == redis.ErrNil {
+			return nil, nil
+		}
+		return nil, e
+	}
+	if len(data) == 0 {
+		//this is empty key
+		return nil, ecode.ErrUserNotExist
+	}
+	user := &model.User{}
+	if e = json.Unmarshal(data, user); e != nil {
+		return nil, e
+	}
+	return user, nil
+}
+func (d *Dao) RedisDelUser(ctx context.Context, userid string) error {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return e
+	}
+	defer c.Close()
+	_, e = redis.String(c.DoContext(ctx, "DEL", userid))
+	return e
+}
+func (d *Dao) RedisSetUserIndexTel(ctx context.Context, tel string, userid string) error {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return e
+	}
+	defer c.Close()
+	_, e = redis.String(c.DoContext(ctx, "SET", tel, userid, "EX", 604800))
+	return e
+}
+func (d *Dao) RedisGetUserIndexTel(ctx context.Context, tel string) (string, error) {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return "", e
+	}
+	defer c.Close()
+	userid, e := redis.String(c.DoContext(ctx, "GET", tel))
+	if e != nil {
+		if e == redis.ErrNil {
+			return "", nil
+		}
+		return "", e
+	}
+	if userid == "" {
+		//this is empty key
+		return "", ecode.ErrUserNotExist
+	}
+	return userid, nil
+}
+func (d *Dao) RedisDelUserIndexTel(ctx context.Context, tel string) error {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return e
+	}
+	defer c.Close()
+	_, e = redis.String(c.DoContext(ctx, "DEL", tel))
+	return e
+}
+func (d *Dao) RedisSetUserIndexEmail(ctx context.Context, email string, userid string) error {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return e
+	}
+	defer c.Close()
+	_, e = redis.String(c.DoContext(ctx, "SET", email, userid, "EX", 604800))
+	return e
+}
+func (d *Dao) RedisGetUserIndexEmail(ctx context.Context, email string) (string, error) {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return "", e
+	}
+	defer c.Close()
+	userid, e := redis.String(c.DoContext(ctx, "GET", email))
+	if e != nil {
+		if e == redis.ErrNil {
+			return "", nil
+		}
+		return "", e
+	}
+	if userid == "" {
+		//this is empty key
+		return "", ecode.ErrUserNotExist
+	}
+	return userid, nil
+}
+func (d *Dao) RedisDelUserIndexEmail(ctx context.Context, email string) error {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return e
+	}
+	defer c.Close()
+	_, e = redis.String(c.DoContext(ctx, "DEL", email))
+	return e
+}
+func (d *Dao) RedisSetUserIndexIDCard(ctx context.Context, idcard string, userid string) error {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return e
+	}
+	defer c.Close()
+	_, e = redis.String(c.DoContext(ctx, "SET", idcard, userid, "EX", 604800))
+	return e
+}
+func (d *Dao) RedisGetUserIndexIDCard(ctx context.Context, idcard string) (string, error) {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return "", e
+	}
+	defer c.Close()
+	userid, e := redis.String(c.DoContext(ctx, "GET", idcard))
+	if e != nil {
+		if e == redis.ErrNil {
+			return "", nil
+		}
+		return "", e
+	}
+	if userid == "" {
+		//this is empty key
+		return "", ecode.ErrUserNotExist
+	}
+	return userid, nil
+}
+func (d *Dao) RedisDelUserIndexIDCard(ctx context.Context, idcard string) error {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return e
+	}
+	defer c.Close()
+	_, e = redis.String(c.DoContext(ctx, "DEL", idcard))
+	return e
+}
+func (d *Dao) RedisSetUserIndexNickName(ctx context.Context, nickname string, userid string) error {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return e
+	}
+	defer c.Close()
+	_, e = redis.String(c.DoContext(ctx, "SET", nickname, userid, "EX", 604800))
+	return e
+}
+func (d *Dao) RedisGetUserIndexNickName(ctx context.Context, nickname string) (string, error) {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return "", e
+	}
+	defer c.Close()
+	userid, e := redis.String(c.DoContext(ctx, "GET", nickname))
+	if e != nil {
+		if e == redis.ErrNil {
+			return "", nil
+		}
+		return "", e
+	}
+	if userid == "" {
+		//this is empty key
+		return "", ecode.ErrUserNotExist
+	}
+	return userid, nil
+}
+func (d *Dao) RedisDelUserIndexNickName(ctx context.Context, nickname string) error {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return e
+	}
+	defer c.Close()
+	_, e = redis.String(c.DoContext(ctx, "DEL", nickname))
 	return e
 }
---
+++ dao/user/redis_code.go
@@ -0,0 +1,135 @@
+package user
+
+import (
+	"context"
+	"crypto/sha1"
+	"encoding/hex"
+	"strings"
+
+	"github.com/chenjie199234/Corelib/redis"
+	"github.com/chenjie199234/account/ecode"
+)
+
+const DefaultExpireSeconds = 300
+const DefaultCheckTimes = 5
+
+func init() {
+	h := sha1.Sum([]byte(setcode))
+	hsetcode = hex.EncodeToString(h[:])
+	h = sha1.Sum([]byte(checkcode))
+	hcheckcode = hex.EncodeToString(h[:])
+}
+
+func rediskey(target, action string) string {
+	return target + "_" + action + "_code"
+}
+
+// argv 1 = code
+// argc 2 = max check times
+// argc 3 = expire time(unit second)
+// return -1 = set success
+// return >=0 = already setted before,data is the rest check times
+const setcode = `local used=redis.call("HGET",KEYS[1],"check")
+if(used~=nil)
+then
+	return ARGV[2]-used
+end
+redis.call("HMSET",KEYS[1],"code",ARGV[1],"check",0)
+redis.call("EXPIRE",KEYS[1],ARGV[2])
+return -1`
+
+var hsetcode string
+
+func (d *Dao) RedisSetCode(ctx context.Context, target, action string, code string) (int, error) {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return 0, e
+	}
+	defer c.Close()
+	var rest int
+	rest, e = redis.Int(c.DoContext(ctx, "EVALSHA", hsetcode, 1, rediskey(target, action), code, DefaultCheckTimes, DefaultExpireSeconds))
+	if e != nil && strings.Contains(e.Error(), "NOSCRIPT") {
+		rest, e = redis.Int(c.DoContext(ctx, "EVAL", setcode, 1, rediskey(target, action), code, DefaultCheckTimes, DefaultExpireSeconds))
+	}
+	if e != nil {
+		return 0, e
+	}
+	if rest != -1 {
+		e = ecode.ErrCodeAlreadySend
+	} else {
+		rest = DefaultCheckTimes
+	}
+	return rest, e
+}
+
+// argv 1 = code
+// argv 2 = max check times
+// argv 3 = expire time(unit second)
+// return nil = key already expired
+// return number = rest check times
+// -1 means code same,check success
+// 0 means all check times used,this key will be expired after expire time
+const checkcode = `local data=redis.call("HMGET",KEYS[1],"code","check")
+if(data[1]==nil or data[2]==nil)
+then
+	return nil
+end
+if(data[1]==ARGV[1] and data[2]<ARGV[2])
+then
+	redis.call("DEL",KEYS[1])
+	return -1
+end
+if(data[2]>=ARGV[2])
+then
+	return 0
+end
+if(redis.call("EXPIRE",KEYS[1],ARGV[2])==0)
+then
+	return nil
+end
+data[2]=redis.call("HINCRBY",KEYS[1],"check",1)
+return ARGV[2]-data[2]`
+
+var hcheckcode string
+
+func (d *Dao) RedisCheckCode(ctx context.Context, target, action string, code string) (int, error) {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return 0, e
+	}
+	defer c.Close()
+	var rest int
+	rest, e = redis.Int(c.DoContext(ctx, "EVALSHA", hcheckcode, 1, rediskey(target, action), code, DefaultCheckTimes, DefaultExpireSeconds))
+	if e != nil && strings.Contains(e.Error(), "NOSCRIPT") {
+		rest, e = redis.Int(c.DoContext(ctx, "EVAL", checkcode, 1, rediskey(target, action), code, DefaultCheckTimes, DefaultExpireSeconds))
+	}
+	if e == redis.ErrNil {
+		e = ecode.ErrCodeAlreadyExpire
+	}
+	return rest, e
+}
+func (d *Dao) RedisGetCode(ctx context.Context, target, action string) (string, int, error) {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return "", 0, e
+	}
+	defer c.Close()
+	values, e := redis.Values(c.DoContext(ctx, "HMGET", rediskey(target, action), "code", "check"))
+	if e != nil {
+		if e == redis.ErrNil {
+			e = ecode.ErrCodeAlreadyExpire
+		}
+		return "", 0, e
+	}
+	return values[0].(string), values[1].(int), nil
+}
+
+func (d *Dao) RedisDelCode(ctx context.Context, target, action string) error {
+	c, e := d.redis.GetContext(ctx)
+	if e != nil {
+		return e
+	}
+	defer c.Close()
+	_, e = c.DoContext(ctx, "DEL", rediskey(target, action))
+	return e
+}
