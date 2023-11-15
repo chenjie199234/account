0b5f41001c48779cbe0dd5c960e8e73eadaecf36
chenjie199234 <chenjie199234@outlook.com>
2023-11-15 15:40:46 +0800

    update

--- util/oauth.go
+++ util/oauth.go
@@ -6,10 +6,10 @@ import (
 	"github.com/chenjie199234/account/ecode"
 )
 
+// caller is the parent function name,this is used for the log
 func OAuthVerifyCode(ctx context.Context, caller string, oauthservicename, code string) (oauthid string, e error) {
 	switch oauthservicename {
 	default:
 		return "", ecode.ErrOAuthUnknown
 	}
-	return
 }
