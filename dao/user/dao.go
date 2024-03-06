704cf6d81e574c9dd9be65ddc4d438ec9b1a8fd5
chenjie199234 <chenjie199234@outlook.com>
2024-03-06 18:24:33 +0800

    update

--- dao/user/dao.go
+++ dao/user/dao.go
@@ -38,16 +38,10 @@ func NewDao(mysql *cmysql.Client, redis *credis.Client, mongo *cmongo.Client) *D
 // user
 
 func (d *Dao) GetUser(ctx context.Context, userid primitive.ObjectID) (*model.User, error) {
-	if user, e := d.RedisGetUser(ctx, userid.Hex()); e != nil {
-		if e == ecode.ErrUserNotExist {
-			//key exist but value is empty
-			return nil, e
-		}
-		if e != gredis.Nil {
-			log.Error(ctx, "[dao.GetUser] redis op failed", log.String("user_id", userid.Hex()), log.CError(e))
-		}
-	} else if user != nil {
-		return user, nil
+	if user, e := d.RedisGetUser(ctx, userid.Hex()); e == nil || e == ecode.ErrUserNotExist {
+		return user, e
+	} else if e != nil && e != gredis.Nil {
+		log.Error(ctx, "[dao.GetUser] redis op failed", log.String("user_id", userid.Hex()), log.CError(e))
 	}
 	//redis error or redis not exist,we need to query db
 	unsafeUser, e := oneshot.Do("GetUser_"+userid.Hex(), func() (unsafe.Pointer, error) {
@@ -72,19 +66,16 @@ func (d *Dao) GetUser(ctx context.Context, userid primitive.ObjectID) (*model.Us
 		}()
 		return unsafe.Pointer(user), e
 	})
-	return (*model.User)(unsafeUser), e
+	if e != nil {
+		return nil, e
+	}
+	return (*model.User)(unsafeUser), nil
 }
 func (d *Dao) GetUserByOAuth(ctx context.Context, oauthservicename, oauthid string) (*model.User, error) {
-	if user, e := d.RedisGetUserByOAuth(ctx, oauthservicename, oauthid); e != nil {
-		if e == ecode.ErrUserNotExist {
-			//key exist but value is empty
-			return nil, e
-		}
-		if e != gredis.Nil && e != ecode.ErrCacheDataConflict {
-			log.Error(ctx, "[dao.GetUserByOAuth] redis op failed", log.String(oauthservicename, oauthid), log.CError(e))
-		}
-	} else if user != nil {
+	if user, e := d.RedisGetUserByOAuth(ctx, oauthservicename, oauthid); e == nil || e == ecode.ErrUserNotExist {
 		return user, nil
+	} else if e != nil && e != gredis.Nil && e != ecode.ErrCacheDataConflict {
+		log.Error(ctx, "[dao.GetUserByOAuth] redis op failed", log.String(oauthservicename, oauthid), log.CError(e))
 	}
 	//redis error or redis not exist,we need to query db
 	unsafeUser, e := oneshot.Do("GetUserByOAuth_"+oauthservicename+"|"+oauthid, func() (unsafe.Pointer, error) {
@@ -123,15 +114,16 @@ func (d *Dao) GetUserByOAuth(ctx context.Context, oauthservicename, oauthid stri
 		}()
 		return unsafe.Pointer(user), e
 	})
-	return (*model.User)(unsafeUser), e
+	if e != nil {
+		return nil, e
+	}
+	return (*model.User)(unsafeUser), nil
 }
 func (d *Dao) GetOrCreateUserByOAuth(ctx context.Context, oauthservicename, oauthid string) (*model.User, error) {
-	if user, e := d.RedisGetUserByOAuth(ctx, oauthservicename, oauthid); e != nil {
-		if e != gredis.Nil && e != ecode.ErrUserNotExist && e != ecode.ErrCacheDataConflict {
-			log.Error(ctx, "[dao.GetOrCreateUserByOAuth] redis op failed", log.String(oauthservicename, oauthid), log.CError(e))
-		}
-	} else if user != nil {
+	if user, e := d.RedisGetUserByOAuth(ctx, oauthservicename, oauthid); e == nil {
 		return user, nil
+	} else if e != nil && e != gredis.Nil && e != ecode.ErrUserNotExist && e != ecode.ErrCacheDataConflict {
+		log.Error(ctx, "[dao.GetOrCreateUserByOAuth] redis op failed", log.String(oauthservicename, oauthid), log.CError(e))
 	}
 	//redis error or redis not exist,we need to query db
 	unsafeUser, e := oneshot.Do("GetOrCreateUserByOAuth_"+oauthservicename+"|"+oauthid, func() (unsafe.Pointer, error) {
@@ -162,19 +154,16 @@ func (d *Dao) GetOrCreateUserByOAuth(ctx context.Context, oauthservicename, oaut
 		}()
 		return unsafe.Pointer(user), nil
 	})
-	return (*model.User)(unsafeUser), e
+	if e != nil {
+		return nil, e
+	}
+	return (*model.User)(unsafeUser), nil
 }
 func (d *Dao) GetUserByTel(ctx context.Context, tel string) (*model.User, error) {
-	if user, e := d.RedisGetUserByTel(ctx, tel); e != nil {
-		if e == ecode.ErrUserNotExist {
-			//key exist but value is empty
-			return nil, e
-		}
-		if e != gredis.Nil && e != ecode.ErrCacheDataConflict {
-			log.Error(ctx, "[dao.GetUserByTel] redis op failed", log.String("tel", tel), log.CError(e))
-		}
-	} else if user != nil {
-		return user, nil
+	if user, e := d.RedisGetUserByTel(ctx, tel); e == nil || e == ecode.ErrUserNotExist {
+		return user, e
+	} else if e != nil && e != gredis.Nil && e != ecode.ErrCacheDataConflict {
+		log.Error(ctx, "[dao.GetUserByTel] redis op failed", log.String("tel", tel), log.CError(e))
 	}
 	//redis error or redis not exist,we need to query db
 	unsafeUser, e := oneshot.Do("GetUserByTel_"+tel, func() (unsafe.Pointer, error) {
@@ -213,15 +202,16 @@ func (d *Dao) GetUserByTel(ctx context.Context, tel string) (*model.User, error)
 		}()
 		return unsafe.Pointer(user), e
 	})
-	return (*model.User)(unsafeUser), e
+	if e != nil {
+		return nil, e
+	}
+	return (*model.User)(unsafeUser), nil
 }
 func (d *Dao) GetOrCreateUserByTel(ctx context.Context, tel string) (*model.User, error) {
-	if user, e := d.RedisGetUserByTel(ctx, tel); e != nil {
-		if e != gredis.Nil && e != ecode.ErrUserNotExist && e != ecode.ErrCacheDataConflict {
-			log.Error(ctx, "[dao.GetOrCreateUserByTel] redis op failed", log.String("tel", tel), log.CError(e))
-		}
-	} else if user != nil {
+	if user, e := d.RedisGetUserByTel(ctx, tel); e == nil {
 		return user, nil
+	} else if e != nil && e != gredis.Nil && e != ecode.ErrUserNotExist && e != ecode.ErrCacheDataConflict {
+		log.Error(ctx, "[dao.GetOrCreateUserByTel] redis op failed", log.String("tel", tel), log.CError(e))
 	}
 	//redis error or redis not exist,we need to query db
 	unsafeUser, e := oneshot.Do("GetOrCreateUserByTel_"+tel, func() (unsafe.Pointer, error) {
@@ -252,19 +242,16 @@ func (d *Dao) GetOrCreateUserByTel(ctx context.Context, tel string) (*model.User
 		}()
 		return unsafe.Pointer(user), nil
 	})
-	return (*model.User)(unsafeUser), e
+	if e != nil {
+		return nil, e
+	}
+	return (*model.User)(unsafeUser), nil
 }
 func (d *Dao) GetUserByEmail(ctx context.Context, email string) (*model.User, error) {
-	if user, e := d.RedisGetUserByEmail(ctx, email); e != nil {
-		if e == ecode.ErrUserNotExist {
-			//key exist but value is empty
-			return nil, e
-		}
-		if e != gredis.Nil && e != ecode.ErrCacheDataConflict {
-			log.Error(ctx, "[dao.GetUserByEmail] redis op failed", log.String("email", email), log.CError(e))
-		}
-	} else if user != nil {
-		return user, nil
+	if user, e := d.RedisGetUserByEmail(ctx, email); e == nil || e == ecode.ErrUserNotExist {
+		return user, e
+	} else if e != nil && e != gredis.Nil && e != ecode.ErrCacheDataConflict {
+		log.Error(ctx, "[dao.GetUserByEmail] redis op failed", log.String("email", email), log.CError(e))
 	}
 	//redis error or redis not exist,we need to query db
 	unsafeUser, e := oneshot.Do("GetUserByEmail_"+email, func() (unsafe.Pointer, error) {
@@ -303,15 +290,16 @@ func (d *Dao) GetUserByEmail(ctx context.Context, email string) (*model.User, er
 		}()
 		return unsafe.Pointer(user), e
 	})
-	return (*model.User)(unsafeUser), e
+	if e != nil {
+		return nil, e
+	}
+	return (*model.User)(unsafeUser), nil
 }
 func (d *Dao) GetOrCreateUserByEmail(ctx context.Context, email string) (*model.User, error) {
-	if user, e := d.RedisGetUserByEmail(ctx, email); e != nil {
-		if e != gredis.Nil && e != ecode.ErrUserNotExist && e != ecode.ErrCacheDataConflict {
-			log.Error(ctx, "[dao.GetOrCreateUserByEmail] redis op failed", log.String("email", email), log.CError(e))
-		}
-	} else if user != nil {
+	if user, e := d.RedisGetUserByEmail(ctx, email); e == nil {
 		return user, nil
+	} else if e != nil && e != gredis.Nil && e != ecode.ErrUserNotExist && e != ecode.ErrCacheDataConflict {
+		log.Error(ctx, "[dao.GetOrCreateUserByEmail] redis op failed", log.String("email", email), log.CError(e))
 	}
 	//redis error or redis not exist,we need to query db
 	unsafeUser, e := oneshot.Do("GetOrCreateUserByEmail_"+email, func() (unsafe.Pointer, error) {
@@ -342,19 +330,16 @@ func (d *Dao) GetOrCreateUserByEmail(ctx context.Context, email string) (*model.
 		}()
 		return unsafe.Pointer(user), nil
 	})
-	return (*model.User)(unsafeUser), e
+	if e != nil {
+		return nil, e
+	}
+	return (*model.User)(unsafeUser), nil
 }
 func (d *Dao) GetUserByIDCard(ctx context.Context, idcard string) (*model.User, error) {
-	if user, e := d.RedisGetUserByIDCard(ctx, idcard); e != nil {
-		if e == ecode.ErrUserNotExist {
-			//key exist but value is empty
-			return nil, e
-		}
-		if e != gredis.Nil && e != ecode.ErrCacheDataConflict {
-			log.Error(ctx, "[dao.GetUserByIDCard] redis op failed", log.String("idcard", idcard), log.CError(e))
-		}
-	} else if user != nil {
-		return user, nil
+	if user, e := d.RedisGetUserByIDCard(ctx, idcard); e == nil || e == ecode.ErrUserNotExist {
+		return user, e
+	} else if e != nil && e != gredis.Nil && e != ecode.ErrCacheDataConflict {
+		log.Error(ctx, "[dao.GetUserByIDCard] redis op failed", log.String("idcard", idcard), log.CError(e))
 	}
 	//redis error or redis not exist,we need to query db
 	unsafeUser, e := oneshot.Do("GetUserByIDCard_"+idcard, func() (unsafe.Pointer, error) {
@@ -376,7 +361,7 @@ func (d *Dao) GetUserByIDCard(ctx context.Context, idcard string) (*model.User, 
 						}
 					}()
 				}
-				return nil, ecode.ReturnEcode(e, ecode.ErrSystem)
+				return nil, e
 			}
 			break
 		}
@@ -393,37 +378,32 @@ func (d *Dao) GetUserByIDCard(ctx context.Context, idcard string) (*model.User, 
 		}()
 		return unsafe.Pointer(user), e
 	})
-	return (*model.User)(unsafeUser), e
+	if e != nil {
+		return nil, e
+	}
+	return (*model.User)(unsafeUser), nil
 }
 
 // oauth index
 func (d *Dao) GetUserOAuthIndex(ctx context.Context, oauthservicename, oauthid string) (string, error) {
-	if userid, e := d.RedisGetUserOAuthIndex(ctx, oauthservicename, oauthid); e != nil {
-		if e == ecode.ErrUserNotExist {
-			//key exist but value is empty
-			return userid, e
-		}
-		if e != gredis.Nil {
-			log.Error(ctx, "[dao.GetUserOAuthIndex] redis op failed", log.String(oauthservicename, oauthid), log.CError(e))
-		}
-	} else if userid != "" {
-		return userid, nil
+	if userid, e := d.RedisGetUserOAuthIndex(ctx, oauthservicename, oauthid); e == nil || e == ecode.ErrUserNotExist {
+		return userid, e
+	} else if e != nil && e != gredis.Nil {
+		log.Error(ctx, "[dao.GetUserOAuthIndex] redis op failed", log.String(oauthservicename, oauthid), log.CError(e))
 	}
 	unsafeUserid, e := oneshot.Do("GetUserOAuthIndex_"+oauthservicename+"|"+oauthid, func() (unsafe.Pointer, error) {
 		index, e := d.MongoGetUserOAuthIndex(ctx, oauthservicename, oauthid)
 		if e != nil {
 			log.Error(nil, "[dao.GetUserOAuthIndex] db op failed", log.String(oauthservicename, oauthid), log.CError(e))
-			if e == ecode.ErrUserNotExist {
-				//set redis empty key
-				go func() {
-					if e := d.RedisSetUserOAuthIndex(context.Background(), oauthservicename, oauthid, ""); e != nil {
-						log.Error(nil, "[dao.GetUserOAuthIndex] update redis failed", log.String(oauthservicename, oauthid), log.CError(e))
-					}
-				}()
+			if e != ecode.ErrUserNotExist {
+				return nil, e
 			}
-			return nil, e
+			//if the error is ErrUserNotExist,set the empty value in redis below
 		}
-		userid := index.UserID.Hex()
+		var userid string
+		if e == nil {
+			userid = index.UserID.Hex()
+		}
 		//update redis
 		go func() {
 			if e := d.RedisSetUserOAuthIndex(context.Background(), oauthservicename, oauthid, userid); e != nil {
@@ -440,33 +420,25 @@ func (d *Dao) GetUserOAuthIndex(ctx context.Context, oauthservicename, oauthid s
 
 // tel index
 func (d *Dao) GetUserTelIndex(ctx context.Context, tel string) (string, error) {
-	if userid, e := d.RedisGetUserTelIndex(ctx, tel); e != nil {
-		if e == ecode.ErrUserNotExist {
-			//key exist but value is empty
-			return userid, e
-		}
-		if e != gredis.Nil {
-			log.Error(ctx, "[dao.GetUserTelIndex] redis op failed", log.String("tel", tel), log.CError(e))
-		}
-	} else if userid != "" {
-		return userid, nil
+	if userid, e := d.RedisGetUserTelIndex(ctx, tel); e == nil || e == ecode.ErrUserNotExist {
+		return userid, e
+	} else if e != nil && e != gredis.Nil {
+		log.Error(ctx, "[dao.GetUserTelIndex] redis op failed", log.String("tel", tel), log.CError(e))
 	}
 	//redis error or redis not exist,we need to query db
 	unsafeUserid, e := oneshot.Do("GetUserTelIndex_"+tel, func() (unsafe.Pointer, error) {
 		index, e := d.MongoGetUserTelIndex(ctx, tel)
 		if e != nil {
 			log.Error(nil, "[dao.GetUserTelIndex] db op failed", log.String("tel", tel), log.CError(e))
-			if e == ecode.ErrUserNotExist {
-				//set redis empty key
-				go func() {
-					if e := d.RedisSetUserTelIndex(context.Background(), tel, ""); e != nil {
-						log.Error(nil, "[dao.GetUserTelIndex] update redis failed", log.String("tel", tel), log.CError(e))
-					}
-				}()
+			if e != ecode.ErrUserNotExist {
+				return nil, e
 			}
-			return nil, e
+			//if the error is ErrUserNotExist,set the empty value in redis below
 		}
-		userid := index.UserID.Hex()
+		var userid string
+		if e == nil {
+			userid = index.UserID.Hex()
+		}
 		//update redis
 		go func() {
 			if e := d.RedisSetUserTelIndex(context.Background(), tel, userid); e != nil {
@@ -483,33 +455,25 @@ func (d *Dao) GetUserTelIndex(ctx context.Context, tel string) (string, error) {
 
 // email index
 func (d *Dao) GetUserEmailIndex(ctx context.Context, email string) (string, error) {
-	if userid, e := d.RedisGetUserEmailIndex(ctx, email); e != nil {
-		if e == ecode.ErrUserNotExist {
-			//key exist but value is empty
-			return userid, e
-		}
-		if e != gredis.Nil {
-			log.Error(ctx, "[dao.GetUserEmailIndex] redis op failed", log.String("email", email), log.CError(e))
-		}
-	} else if userid != "" {
-		return userid, nil
+	if userid, e := d.RedisGetUserEmailIndex(ctx, email); e == nil || e == ecode.ErrUserNotExist {
+		return userid, e
+	} else if e != nil && e != gredis.Nil {
+		log.Error(ctx, "[dao.GetUserEmailIndex] redis op failed", log.String("email", email), log.CError(e))
 	}
 	//redis error or redis not exist,we need to query db
 	unsafeUserid, e := oneshot.Do("GetUserEmailIndex_"+email, func() (unsafe.Pointer, error) {
 		index, e := d.MongoGetUserEmailIndex(ctx, email)
 		if e != nil {
 			log.Error(nil, "[dao.GetUserEmailIndex] db op failed", log.String("email", email), log.CError(e))
-			if e == ecode.ErrUserNotExist {
-				//set redis empty key
-				go func() {
-					if e := d.RedisSetUserEmailIndex(context.Background(), email, ""); e != nil {
-						log.Error(nil, "[dao.GetUserEmailIndex] update redis failed", log.String("email", email), log.CError(e))
-					}
-				}()
+			if e != ecode.ErrUserNotExist {
+				return nil, e
 			}
-			return nil, e
+			//if the error is ErrUserNotExist,set the empty value in redis below
 		}
-		userid := index.UserID.Hex()
+		var userid string
+		if e == nil {
+			userid = index.UserID.Hex()
+		}
 		//update redis
 		go func() {
 			if e := d.RedisSetUserEmailIndex(context.Background(), email, userid); e != nil {
@@ -526,33 +490,25 @@ func (d *Dao) GetUserEmailIndex(ctx context.Context, email string) (string, erro
 
 // idcard index
 func (d *Dao) GetUserIDCardIndex(ctx context.Context, idcard string) (string, error) {
-	if userid, e := d.RedisGetUserIDCardIndex(ctx, idcard); e != nil {
-		if e == ecode.ErrUserNotExist {
-			//key exist but value is empty
-			return userid, e
-		}
-		if e != gredis.Nil {
-			log.Error(ctx, "[dao.GetUserIDCardIndex] redis op failed", log.String("idcard", idcard), log.CError(e))
-		}
-	} else if userid != "" {
-		return userid, nil
+	if userid, e := d.RedisGetUserIDCardIndex(ctx, idcard); e == nil || e == ecode.ErrUserNotExist {
+		return userid, e
+	} else if e != nil && e != gredis.Nil {
+		log.Error(ctx, "[dao.GetUserIDCardIndex] redis op failed", log.String("idcard", idcard), log.CError(e))
 	}
 	//redis error or redis not exist,we need to query db
 	unsafeUserid, e := oneshot.Do("GetUserIDcardIndex_"+idcard, func() (unsafe.Pointer, error) {
 		index, e := d.MongoGetUserIDCardIndex(ctx, idcard)
 		if e != nil {
 			log.Error(nil, "[dao.GetUserIDCardIndex] db op failed", log.String("idcard", idcard), log.CError(e))
-			if e == ecode.ErrUserNotExist {
-				//set redis empty key
-				go func() {
-					if e := d.RedisSetUserIDCardIndex(context.Background(), idcard, ""); e != nil {
-						log.Error(nil, "[dao.GetUserIDCardIndex] update redis failed", log.String("idcard", idcard))
-					}
-				}()
+			if e != ecode.ErrUserNotExist {
+				return nil, e
 			}
-			return nil, e
+			//if the error is ErrUserNotExist,set the empty value in redis below
 		}
-		userid := index.UserID.Hex()
+		var userid string
+		if e == nil {
+			userid = index.UserID.Hex()
+		}
 		//update redis
 		go func() {
 			if e := d.RedisSetUserIDCardIndex(context.Background(), idcard, userid); e != nil {
