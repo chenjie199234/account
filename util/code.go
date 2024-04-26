acd6180fec5ae8e048403fddce602dd2f1cd9a96
chenjie199234 <chenjie199234@outlook.com>
2024-04-26 14:49:11 +0800

    update

--- util/code.go
+++ util/code.go
@@ -2,8 +2,6 @@ package util
 
 import (
 	"context"
-	"net/smtp"
-	"strings"
 
 	"github.com/chenjie199234/account/config"
 
@@ -11,20 +9,8 @@ import (
 )
 
 func SendEmailCode(ctx context.Context, email, code, action string) error {
-	c := config.AC.Service
-	host := ""
-	if index := strings.LastIndex(c.EmailSMTPAddr, ":"); index == -1 {
-		host = c.EmailSMTPAddr
-	} else {
-		host = c.EmailSMTPAddr[:index]
-	}
-	msg := []string{"From: " + c.EmailAccount, "To: " + email, "Subject: Login Dynamic Code\r\n", "Your dynamic code is:" + code}
-	msgstr := strings.Join(msg, "\r\n")
-	if host == c.EmailSMTPAddr {
-		//use the default port 587
-		return smtp.SendMail(c.EmailSMTPAddr+":587", smtp.PlainAuth("", c.EmailAccount, c.EmailPassword, host), c.EmailAccount, []string{email}, common.STB(msgstr))
-	}
-	return smtp.SendMail(c.EmailSMTPAddr, smtp.PlainAuth("", c.EmailAccount, c.EmailPassword, host), c.EmailAccount, []string{email}, common.STB(msgstr))
+	body := "Your dynamic code is:" + code
+	return config.GetEmail("qq_email").SendTextEmail(ctx, []string{email}, "Login Dynamic Code", common.STB(body))
 }
 
 // TODO
